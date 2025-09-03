2417f8118b8927e4a4b3097c9ecc9965903c8f42
danlawless <danieljlawless@gmail.com>
2025-09-03 13:29:50 -0400

    Add Universal Editor integration to homepage
    - Added editor embed script to root layout
    - Editor will be accessible at /editor/ with password 'universal2025!'
    - Configured for Busy Bees branding and colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
     <html lang="en" className={inter.variable}>
       <body className={`${inter.className} font-body antialiased`}>
         {children}
+        <script src="/editor/embed/editor-embed.js" data-password="universal2025!" data-debug="true" async></script>
       </body>
     </html>
   );
